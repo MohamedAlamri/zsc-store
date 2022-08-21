6128f300e2c58d8b07e077de2ebf8dbb625bae35
MohamedAlamri <99891370+MohamedAlamri@users.noreply.github.com>
2022-08-21 23:04:25 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,5 +1,5 @@
 let basket = JSON.parse(localStorage.getItem("Data")) || [];
-let container = document.querySelector(".cart .container")
+let container = document.querySelector(".cart-page .container")
 let cart = document.querySelector(".product-cart")
 
 
@@ -27,9 +27,22 @@ let gemeratCartItems = () => {
 
         return (
           cart.innerHTML = `
-            <img src=${search.img} >
-            <h3>${search.name}</h3>
-            <h4>$ ${search.price}</h4>
+            <div class="cart-item" >
+                <img src=${search.img} >
+                <div class="info" >
+                  <div class="name-price">
+                      <h3 class="name-product">${search.name}</h3>
+                      <h4 class="price-product">$ ${search.price}</h4>
+                      <i onclick="removeItem(${id})" class="fa-solid fa-xmark"></i>
+                  </div>
+                    <div class="quantity-c">
+                      <i onclick="decrement(${id})" class="fa-solid fa-minus"></i>
+                      <span class="quantity" id=${id}>${item}</span>
+                      <i onclick="increment(${id})" class="fa-solid fa-plus"></i>
+                    </div>
+                    <h3>$ ${item * search.price}</h3>
+                </div>
+            </div>
           `
         )
           
@@ -38,8 +51,12 @@ let gemeratCartItems = () => {
 }else {
   return(
     container.innerHTML = `
-      <h2>The cart is empty</h2>
-      
+    <div class="empty" >
+        <h2>cart is empty</h2>
+        <button>
+          <a href="index.html" >Back To Home</a>
+        </button>
+      </div>
       `
   )
 }
@@ -48,3 +65,59 @@ let gemeratCartItems = () => {
 }
 calculatio()
 gemeratCartItems()
+
+
+
+
+let increment = (id) => {
+  
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1
+    })
+  }else {
+    search.item += 1;
+  }
+  update(selectedItem.id);
+  localStorage.setItem("Data", JSON.stringify(basket));
+  gemeratCartItems();
+}
+
+
+
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+
+  else {
+    search.item -= 1;
+  }
+
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  gemeratCartItems();
+  localStorage.setItem("Data", JSON.stringify(basket));
+}
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerText = search.item;
+  calculatio()
+}
+
+
+let removeItem = (id) => {
+  let selectedItem = id
+
+  basket = basket.filter((x) => x.id !== selectedItem.id);
+  localStorage.setItem("Data", JSON.stringify(basket));
+  gemeratCartItems();
+
+}
